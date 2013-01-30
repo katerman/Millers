94e537d75e5a35b5b800f5515f38c984bfc9ccf4
Kevin Reynolds <katermanlw@gmail.com>
2013-01-30 16:28:12 -0500

    Redesign - updated to html5 - 1.0
    Redesign for millers site, the old design wasnt working for me any
    longer, and now its a little bit better.

--- MillersHomeOwnerHelper/message.php
+++ MillersHomeOwnerHelper/message.php
@@ -11,6 +11,7 @@ $message = $_POST['message'];
 //Validate first
 if(empty($name)||empty($visitor_email)) 
 {
+	echo "<a href='contact.html'>&lt;Back</a><br/>";
     echo "Name and email are mandatory!";
     exit;
 }
@@ -32,7 +33,7 @@ $headers .= "Reply-To: $visitor_email \r\n";
 //Send the email!
 mail($to,$email_subject,$email_body,$headers);
 //done. redirect to thank-you page.
-header('Location: index.html');
+header('Location: contact.html');
 
 
 // Function to validate against any email injection attempts
